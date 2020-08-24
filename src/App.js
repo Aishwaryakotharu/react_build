63feaba91901ee3b81da8743f56b9d53a3cc756d
AISHWARYA KOTHARU <aishwaryakotharu23@gmail.com>
2020-08-24 14:15:17 +0530

    ohk first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import useFetchTasks from "./useFetchTasks";
+
+export default function App() {
+  const { tasks, isLoading, error } = useFetchTasks(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
+  return (
+    <div className="App">
+      {isLoading ? <p>Loading</p> : null}
+      {tasks.map((task, taskIndex) => {
+        return <p key={taskIndex}>{task.title}</p>;
+      })}
+      {error ? <pre>{JSON.stringify(error)}</pre> : null}
+    </div>
+  );
+}
