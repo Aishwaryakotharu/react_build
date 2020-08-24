8e9eb80c3f49feca527542e69a84091202bf811a
AISHWARYA KOTHARU <aishwaryakotharu23@gmail.com>
2020-08-24 14:52:45 +0530

    Updates

---
+++ precache-manifest.c3ab9edb25bcc292684ef0674bf43252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1577f7950c8bd06f235e5275ec03a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3537e57cd47543fc3890",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "3537e57cd47543fc3890",
+    "url": "/static/js/main.4f24c42b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
